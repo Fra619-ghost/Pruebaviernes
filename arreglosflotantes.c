8be518c27341a03a25e934f37ba04241cc90eac4
Fra619-ghost <fxaaguilera@uamv.edu.ni>
2024-04-12 07:45:00 -0600

    Se agrego la base de datos de flotantes

---
+++ arreglosflotantes.c
@@ -0,0 +1,31 @@
+//Almacenar 6 numeros flotantes
+
+#include <stdio.h>                  // ctrl + alt escribe lo que quieres agrrgar a los valores agregados
+
+int main(int argc, char const *argv[])
+{
+    float numbers[6];
+    numbers[0]= 12;
+    numbers[1]= 1.68;
+    numbers[2]= 2.680;
+    numbers[3]= 3.5;
+    numbers[4]= 1512.9898997;
+    numbers[5]= 13;
+
+    printf("El elemto #1 %f\n",numbers[0]);
+    printf("El elemto #2 %f\n",numbers[1]);
+    printf("El elemto #3 %f\n",numbers[2]);
+    printf("El elemto #4 %f\n",numbers[3]);
+    printf("El elemto #5 %f\n",numbers[4]);
+    printf("El elemto #6 %f\n",numbers[5]);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
---
+++ numerospare.c
@@ -0,0 +1,22 @@
+#include<stdio.h>                  //shift + alt y flecha abajo
+
+int main(int argc, char const *argv[])
+{
+    
+    int numbers[5];
+    numbers[0]=2;
+    numbers[1]=44;
+    numbers[2]=84;
+    numbers[3]=100;
+    numbers[4]=200;
+
+    printf("\nElemento #1: %d",numbers[0]);
+    printf("\nElemento #1: %d",numbers[1]);
+    printf("\nElemento #1: %d",numbers[2]);
+    printf("\nElemento #1: %d",numbers[3]);
+    printf("\nElemento #1: %d",numbers[4]);
+
+
+
+    return 0;
+}
