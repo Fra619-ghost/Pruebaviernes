00628cae7d44432ff62af419caa37522cde7f2f7
Fra619-ghost <fxaaguilera@uamv.edu.ni>
2024-04-12 08:01:29 -0600

    Se agrego un arreglo de letras

---
+++ arreglosdevocales.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char vocals[5];
+    vocals[0]='a';
+    vocals[1]='e';
+    vocals[2]='i';
+    vocals[3]='o';
+    vocals[4]='u';
+
+    printf("\nLa elemento #1 es: %c", vocals[0]);
+    printf("\nLa elemento #2 es: %c", vocals[1]);
+    printf("\nLa elemento #3 es: %c", vocals[2]);
+    printf("\nLa elemento #4 es: %c", vocals[3]);
+    printf("\nLa elemento #5 es: %c", vocals[4]);
+
+
+
+
+
+    return 0;
+}
